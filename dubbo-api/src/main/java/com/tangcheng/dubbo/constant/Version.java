1d9f10b5bd367df05aa69bfa196ae69671d436e3
Tangcheng <helloworld.tang@qq.com>
2017-06-01 22:37:56 +0800

    建议使用两位版本号，因为第三位版本号通常表示兼容升级，只有不兼容时才需要变更服务版本。

--- dubbo-api/src/main/java/com/tangcheng/dubbo/constant/Version.java
+++ dubbo-api/src/main/java/com/tangcheng/dubbo/constant/Version.java
@@ -4,6 +4,6 @@ package com.tangcheng.dubbo.constant;
  * Created by tang.cheng on 2017/6/1.
  */
 public class Version {
-    public static final String V_1_0_0 = "1.0.0";
+    public static final String V_1_0 = "1.0";
 }
 
